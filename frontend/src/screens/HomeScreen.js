ab126436a860e55a9ceb40622a71e63a8add2451
RyanCantCode <kirbyryan36@gmail.com>
2020-10-09 12:17:39 -0400

    added express and the mongoose schemas for backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const fetchProducts = async () => {
+           const { data } = await axios.get('/api/products') 
+           setProducts(data)
+        }
+        fetchProducts()  
+    }, [])
+
     return (
         <>
             <h1>Latest Products</h1>
